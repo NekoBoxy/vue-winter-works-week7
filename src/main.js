d89489e3605ab60c7d4d9d67dd862eb40dbd58a6
CatBoxy <catboxy@gmail.com>
2023-02-13 11:16:19 +0800

    new stules.scss & edit main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+// Import all of Bootstrap's JS
+import * as bootstrap from "bootstrap";
 
 import App from "./App.vue";
 import router from "./router";
 
 import "./assets/main.css";
+// Import our custom CSS
+import "./scss/styles.scss";
 
 const app = createApp(App);
 
