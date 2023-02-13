78451ee6fc39f23c209e319612acf1bd25351860
CatBoxy <catboxy@gmail.com>
2023-02-13 10:52:30 +0800

    bootstrap & sass init

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,26 @@
 <template>
-  <header></header>
+  <div>
+    <HomeView></HomeView>
+  </div>
+  <br />
+  <div>
+    <AboutView></AboutView>
+  </div>
 </template>
 
 <script>
 import { RouterLink, RouterView } from "vue-router";
+import HomeView from "./views/HomeView.vue";
+import AboutView from "./views/AboutView.vue";
 
 export default {
   props: {
     RouterLink,
     RouterView,
+  },
+  components: {
+    HomeView,
+    AboutView,
   },
 };
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,17 @@
 <template>
   <div>it's HomeView. vue</div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      data: {},
+    };
+  },
+  mounted() {
+    console.log(import.meta.env.VITE_URL);
+    console.log(import.meta.env.VITE_PATH);
+  },
+};
+</script>
