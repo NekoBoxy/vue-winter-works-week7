3977196a240762b225fdefa813957777349c885a
CatBoxy <catboxy@gmail.com>
2023-02-15 16:53:18 +0800

    deploy

---
+++ assets/CouponsView-860c2654.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c}from"./index-993dd325.js";const n={};function r(s,t){return o(),c("div",null,"我是後台 coupons :D")}const a=e(n,[["render",r]]);export{a as default};
---
+++ assets/OrdersView-d06c4b24.js
@@ -0,0 +1,1 @@
+import{_ as e,o as r,c}from"./index-993dd325.js";const o={};function n(s,t){return r(),c("div",null,"我是後台 orders")}const a=e(o,[["render",n]]);export{a as default};
