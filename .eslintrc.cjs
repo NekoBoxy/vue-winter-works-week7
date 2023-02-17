69aa0e422c6df1ecd0cabcd92dc9170ebf638db0
CatBoxy <catboxy@gmail.com>
2023-02-17 15:39:59 +0800

    環境設定修改

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,10 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   root: true,
-  extends: ["plugin:vue/vue3-essential", "eslint:recommended"],
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended"
+  ],
   parserOptions: {
     ecmaVersion: "latest",
   },
