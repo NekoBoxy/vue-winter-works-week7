6664af72385a899f0be0cdf2a079fac7918cce6d
CatBoxy <catboxy@gmail.com>
2023-02-21 11:20:11 +0800

    更改為work7

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/vue-winter-works-week6",
+  base: "/vue-winter-works-week7",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
