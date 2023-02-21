84d12ab5db4269d53f0572269bf2283724961ec8
CatBoxy <catboxy@gmail.com>
2023-02-21 11:01:30 +0800

    test

---
+++ src/components/admin/AdminProducts.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>我是後台 AdminProducts :D</div>
+</template>
+
+<script></script>
+
+<style scoped></style>
--- src/views/admin/OrdersView.vue
+++ src/views/admin/OrdersView.vue
@@ -2,7 +2,11 @@
   <div>我是後台 orders</div>
 </template>
 
-<script></script>
+<script>
+import AdminProducts from "src\components\admin\AdminProducts.vue";
+import AdminProducts from "..";
+
+</script>
 
 <style scoped>
 
