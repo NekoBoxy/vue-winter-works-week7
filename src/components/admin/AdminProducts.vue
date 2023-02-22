e5445ac8bad8d51102f705d56ba5c8741325a054
CatBoxy <catboxy@gmail.com>
2023-02-22 14:23:11 +0800

    小調整

--- src/components/admin/AdminProducts.vue
+++ src/components/admin/AdminProducts.vue
@@ -1,53 +1,41 @@
 <template>
-  
-  <table class="table align-middle">
-    <thead>
-      <tr class="text-right">
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td>新增產品</td>
-      </tr>
-      <tr>
-        <td>圖片</td>
-        <td>分類</td>
-        <td>單位(個/unit)</td>
-        <td>原價</td>
-        <td>售價</td>
-        <td>是否啟用</td>
-        <td>編輯</td>
-        <td>刪除</td>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="product in products" v-bind:key="product.id">
-        <td style="width:200px">
-          <div style="height: 100px; background-size: cover; background-position: center"
-            :style="{ 'background-image': `url(${product.imageUrl})` }">
-          </div>
-        </td>
-        <td>{{ product.category }}</td>
-        <td>{{ product.unit }}</td>
-        <td>{{ product.origin_price }}</td>
-        <td>{{ product.price }}</td>
-        <td>{{ product.price.is_enabled }}</td>
-        <td>
-          <button v-on:click="editProduct(id)" class="btn btn-outline-secondary">編輯</button>
-        </td>
-        <td>
-          <button v-on:click="delProduct(id)" class="btn btn-outline-secondary">刪除</button>
-        </td>
-      </tr>
-    </tbody>
-    <thead>
-      
-    </thead>
-  </table>
-
+  <div class="container">
+    <table class="table align-middle">
+      <thead>
+        <tr>
+          <td>圖片</td>
+          <td>分類</td>
+          <td>單位(個/unit)</td>
+          <td>原價</td>
+          <td>售價</td>
+          <td>是否啟用</td>
+          <td>編輯</td>
+          <td>刪除</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="product in products" v-bind:key="product.id">
+          <td style="width:200px">
+            <div style="height: 100px; background-size: cover; background-position: center"
+              :style="{ 'background-image': `url(${product.imageUrl})` }">
+            </div>
+          </td>
+          <td>{{ product.category }}</td>
+          <td>{{ product.unit }}</td>
+          <td>{{ product.origin_price }}</td>
+          <td>{{ product.price }}</td>
+          <td>{{ product.price.is_enabled }}</td>
+          <td>
+            <button v-on:click="editProduct(id)" class="btn btn-outline-secondary">編輯</button>
+          </td>
+          <td>
+            <button v-on:click="delProduct(id)" class="btn btn-outline-secondary">刪除</button>
+          </td>
+        </tr>
+      </tbody>
+      <tfoot></tfoot>
+    </table>
+  </div>
 </template>
 
 <script>
@@ -71,6 +59,7 @@ export default {
 
   },
   methods: {
+    // 取得產品資料
     async getAllProducts() {
       const response = await axios({
         method: "get",
@@ -87,22 +76,20 @@ export default {
     getId() {
 
     },
-    // editProduct(id) {
-
+    // 刪除單筆的功能有錯，還需要修改 
+    // async delProduct(id) {
+    //   const response = await axios({
+    //     method: "delete",
+    //     url: `${import.meta.env.VITE_BASE_URL}/v2/api/${import.meta.env.VITE_BASE_PATH}/admin/product/${id}`,
+    //   }).catch((error) => {
+    //     console.log("error", error);
+    //   });
+    //   console.log(response);
+    //   // this.products = response.data.products;
+    //   // this.pagination = response.data.pagination;
+    //   console.log("已刪除資料");
+    //   // this.getAllProducts();
     // },
-    async delProduct(id) {
-      const response = await axios({
-        method: "delete",
-        url: `${import.meta.env.VITE_BASE_URL}/v2/api/${import.meta.env.VITE_BASE_PATH}/admin/product/${id}`,
-      }).catch((error) => {
-        console.log("error", error);
-      });
-      console.log(response);
-      // this.products = response.data.products;
-      // this.pagination = response.data.pagination;
-      console.log("已刪除資料");
-      // this.getAllProducts();
-    },
   },
   async mounted() {
     await this.getAllProducts();
